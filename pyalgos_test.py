fd4f74657fbf9cc2afd5ad96a7ca50547471bbfa
Derek <djh458@stern.nyu.edu>
2019-05-26 21:01:53 -0400

    finished first draft of interactive module tester. introspection limited to the function level, and limited to having the package in the same directory as this file.

--- pyalgos_test.py
+++ pyalgos_test.py
@@ -3,8 +3,6 @@
 #
 # IMPORTANT: this script should be in the same directory as the pyalgos package!
 #
-# THIS SCRIPT IS DEAD
-#
 # Changelog:
 #
 # 05-26-2019
@@ -12,8 +10,9 @@
 # added implementations for the help function, and started writing code for the
 # test command, which is the main command to be used for testing modules. added
 # functionality for the test command, by using eval to evaluate expressions with
-# the interpreter and return the formatted string output. started work on the
-# info command. 
+# the interpreter and return the formatted string output. added checks for
+# the package being in the same directory as this file. started work on the
+# info command.
 #
 # 05-25-2019
 #
@@ -28,15 +27,19 @@
 # initial creation.
 
 import importlib # dynamic imports
-import inspect # for object introspection
-import textwrap # for wrapping lines
+import inspect # for object introspection (unused except in main)
+import os # for file, misc os stuff
+import pkgutil # for module/package introspection
 import sys
+import textwrap # for wrapping lines
+
+from pyalgos import _PACKAGE_NAME # global package name
 from pyalgos import graph
 
 ### global variables ###
 
-# package name
-_PKGNAME = "pyalgos"
+# package path
+_PACKAGE_PATH = "./"
 
 # program name
 _PROGNAME = "pyalgos_test"
@@ -47,11 +50,13 @@ _HELP_FLAG = "--help"
 # help string
 _HELP_STR = ("Usage: {0} [ {1} ]\n\n"
              "{0} is an interactive script that allows the user to dynamically "
-             "load different modules from the pyalgos package, and run them on "
+             "load different modules from the {2} package, and run them on "
              "different predefined examples for each module. type '{0} {1}' for"
              " this usage.\n\nhelp on the individual commands can be found by "
-             "invoking the 'help' command while in the interpreter."
-             "".format(_PROGNAME, _HELP_FLAG))
+             "invoking the 'help' command while in the interpreter. note that "
+             "the {2} package must be in the same directory as {0}, or else the"
+             " script will not run."
+             "".format(_PROGNAME, _HELP_FLAG, _PACKAGE_NAME))
 
 # useful for indexing command names and help blurbs
 _CNAME = "_cname"
@@ -84,7 +89,7 @@ _COMMAND_LIST = {C_EXIT:
                   _CHELP: ("Usage: {0} [ command ] \n\nReturns the help blurb "
                            "for the specified command. '{0} {0}' returns this "
                            "blurb, while specifying no arguments returns a "
-                           "list of all available commands.".format(C_HELP))},
+                           "list of all available commands.\n".format(C_HELP))},
                   C_TEST:
                   {_CNAME: C_TEST,
                    _CHELP: ("Usage: {0} module_name.function_name < arg1, arg2,"
@@ -95,7 +100,7 @@ _COMMAND_LIST = {C_EXIT:
                             "specified function. arguments may either be "
                             "manually specified, or refer to any predefined"
                             " internal examples whose descriptions may be "
-                            "retrieved with the '{1}' command."
+                            "retrieved with the '{1}' command.\n"
                             "".format(C_TEST, C_INFO))},
                  C_INFO:
                  {_CNAME: C_INFO,
@@ -115,8 +120,9 @@ _COMMAND_LIST = {C_EXIT:
                            "provided in the script. If an input name is "
                            "specified, its associated descriptive blurb will be"
                            " returned. If no input name is specified, then a "
-                           "list of all the sample inputs is returned."
-                           "".format(C_INFO, C_INFO_SRC, C_INFO_INP, _PKGNAME))}
+                           "list of all the sample inputs is returned.\n"
+                           "".format(C_INFO, C_INFO_SRC, C_INFO_INP,
+                                     _PACKAGE_NAME))}
 }
 
 ## graph examples ##
@@ -152,22 +158,24 @@ _EVALUE = "_evalue"
 _EBLURB = "_eblurb"
 
 # dictionary of sample inputs, indexed by variable name.
-_INPUTS_LIST = {u_dag01_al__name:
-                {_ENAME: u_dag01_al__name, _EVALUE: u_dag01_al,
-                 _EBLURB: ("Directed acyclic graph 1, adjacency list form. "
-                           "Has 6 vertices. View:\n{0}".format(u_dag01_al))},
-                u_dag01_am__name:
-                {_ENAME: u_dag01_al__name, _EVALUE: u_dag01_am,
-                 _EBLURB: ("Directed acyclic graph 1, adjacency matrix form"
-                           ". Has 6 vertices. View:\n{0}".format(u_dag01_am))},
-                u_dag02_al__name:
-                {_ENAME: u_dag02_al__name, _EVALUE: u_dag02_al,
-                 _EBLURB: ("Directed acyclic graph 2, adjacency list form. "
-                           "Has 6 vertices. View:\n{0}".format(u_dag02_al))},
-                u_dag02_am__name:
-                {_ENAME: u_dag02_am__name, _EVALUE: u_dag02_am,
-                 _EBLURB: ("Directed acyclic graph 2, adjacency matrix form"
-                           ". Has 6 vertices. View:\n{0}".format(u_dag02_am))}
+_INPUT_LIST = {u_dag01_al__name:
+               {_ENAME: u_dag01_al__name, _EVALUE: u_dag01_al,
+                _EBLURB: ("Directed acyclic graph 1, adjacency list form. "
+                          "Has 6 vertices.\n\nView:\n{0}\n".format(u_dag01_al))},
+               u_dag01_am__name:
+               {_ENAME: u_dag01_al__name, _EVALUE: u_dag01_am,
+                _EBLURB: ("Directed acyclic graph 1, adjacency matrix form"
+                          ". Has 6 vertices.\n\nView:\n{0}\n"
+                          "".format(str(u_dag01_am).replace("],", "],\n")))},
+               u_dag02_al__name:
+               {_ENAME: u_dag02_al__name, _EVALUE: u_dag02_al,
+                _EBLURB: ("Directed acyclic graph 2, adjacency list form. "
+                          "Has 6 vertices.\n\nView:\n{0}\n".format(u_dag02_al))},
+               u_dag02_am__name:
+               {_ENAME: u_dag02_am__name, _EVALUE: u_dag02_am,
+                _EBLURB: ("Directed acyclic graph 2, adjacency matrix form"
+                          ". Has 6 vertices.\n\nView:\n{0}\n"
+                          "".format(str(u_dag02_am).replace("],", "],\n")))}
 }
 
 ### utility functions ###
@@ -295,12 +303,14 @@ def _test(args):
             return
         # make refs
         mref, fref = refs
+        # set absolute module name
+        mref = _PACKAGE_NAME + "." + mref
         # check if the module has already been imported or not (absolute name)
-        try: mref = _IMPORT_LIST[_PKGNAME + "." + mref]
+        try: mref = _IMPORT_LIST[mref]
         except KeyError:
             print("{0}: loading module {1}: ".format(C_TEST, mref), end = "")
             # if it is not found, then try to to import the module
-            try: mref = importlib.import_module(_PKGNAME + "." + mref)
+            try: mref = importlib.import_module(mref)
             except ImportError:
                 print("failed\n{0}: error: unknown module '{1}'"
                       "".format(C_TEST, mref))
@@ -308,9 +318,9 @@ def _test(args):
             except:
                 _ufatal(C_TEST)
                 return
-            print("loaded")
             # if import was successful, key mref into _IMPORT_LIST by its name
             _IMPORT_LIST[mref.__name__] = mref
+            print("loaded")
         # if other error occurred during dictionary lookup
         except:
             _ufatal(C_TEST)
@@ -318,7 +328,7 @@ def _test(args):
         # check if function is in module (is attribute)
         try: fref = getattr(mref, fref)
         except AttributeError:
-            print("{0}: error: unknown function '{1}' not in module '{2}'"
+            print("{0}: error: function '{1}' not found in module '{2}'"
                   "".format(C_TEST, fref, mref.__name__))
             return
         except:
@@ -342,6 +352,162 @@ def _test(args):
     else:
         print("{0}: error: too many arguments. type '{1} {0}' for usage."
               "".format(C_TEST, C_HELP))
+    return
+
+# info command.
+def _info(args):
+    # get number of arguments in args
+    argc = len(args)
+    # if number of elements in args is less than 1, print error
+    if argc < 1:
+        print("{0}: error: not enough arguments. type '{1} {0}' for usage."
+              "".format(C_INFO, C_HELP))
+    # else if there are one or two arguments
+    elif (argc == 1) or (argc == 2):
+        # if the first argument is C_INFO_SRC (module/function stuff)
+        if args[0] == C_INFO_SRC:
+            # if there is only one argument, print a list of the modules
+            if argc == 1:
+                # get module info for all modules in the package
+                modinfo = list(pkgutil.iter_modules([_PACKAGE_PATH +
+                                                     _PACKAGE_NAME]))
+                # for each module, get its indented name and load status
+                for i in range(len(modinfo)):
+                    ment = modinfo[i][1]
+                    # check if this module has been loaded already
+                    try:
+                        _ = _IMPORT_LIST[_PACKAGE_NAME + "." + ment]
+                        # 5 spaces, then 6 spaces
+                        ment = "     True      " + ment
+                    except KeyError:
+                        # 5 spaces, then 5 spaces
+                        ment = "     False     " + ment
+                    except:
+                        _ufatal(C_INFO)
+                        return
+                    modinfo[i] = ment
+                # sort the list and join by newline
+                modinfo.sort()
+                modlist = "\n".join(modinfo)
+                # print out list of modules, including load status
+                print("List of modules:\n\n    loaded?\n{0}".format(modlist))
+            # else there are two arguments, so try to retrieve info on the
+            # requested function/module
+            else:
+                # module reference, function reference
+                mref = fref = None
+                # try to split module and function names apart
+                refs = args[1].split(".")
+                # set absolute module name
+                mref = _PACKAGE_NAME + "." + refs[0]
+                # check if the module has been imported or not (same method
+                # as in the test function that we implemented)
+                try: mref = _IMPORT_LIST[mref]
+                # if key error, then we have to load it
+                except KeyError:
+                    print("{0}: loading module {1}: ".format(C_INFO, mref),
+                          end = "")
+                    # try to import the module and catch any errors
+                    try: mref = importlib.import_module(mref)
+                    except ImportError:
+                        print("failed\n{0}: error: unknown module '{1}'"
+                              "".format(C_INFO, mref))
+                        return
+                    except:
+                        _ufatal(C_INFO)
+                        return
+                    # if import was successful, key mref into _IMPORT_LIST by
+                    # its absolute name
+                    _IMPORT_LIST[mref.__name__] = mref
+                    print("loaded")
+                # handles any other dictionary lookup error
+                except:
+                    _ufatal(C_INFO)
+                    return
+                # if we get to this point, then mref points to the loaded module
+                # if there is only one element in refs, then we care only about
+                # the module, and we return the list of functions associated
+                if len(refs) == 1:
+                    flist = inspect.getmembers(mref, inspect.isfunction)
+                    # for each (name, function pointer) tuple, we extract the
+                    # name and the function signature using inspect
+                    for i in range(len(flist)):
+                        # append 4 spaces as indent too
+                        flist[i] = "    " + flist[i][0] + str(
+                            inspect.signature(flist[i][1]))
+                    # sort and join by newline
+                    flist.sort()
+                    flist = "\n".join(flist)
+                    # print out the wrapped list of functions in the module
+                    print(_strwrap("List of functions in module {0}:\n\n{1}\n"
+                          "".format(mref.__name__, flist)))
+                # else if there are two elements, then this is a function
+                elif len(refs) == 2:
+                    fref = refs[1]
+                    # try to see if this function exists or now
+                    try: fref = getattr(mref, fref)
+                    # if this is not an attribute of the module
+                    except AttributeError:
+                        print("{0}: function '{1}' not found in module '{2}'"
+                              "".format(C_INFO, fref, mref.__name__))
+                        return
+                    except:
+                        _ufatal(C_INFO)
+                        return
+                    # print function signature and docstring (if any)
+                    fdoc = fref.__doc__
+                    if fdoc.strip() == "":
+                        fdoc = "\nNo docstring associated with function {0}\n"
+                        "".format(fref.__name__)
+                    print("Info on function {0} in module {1}:\n\n{0}{2}{3}"
+                          "".format(fref.__name__, mref.__name__,
+                                    str(inspect.signature(fref)), fdoc))
+                # else introspection error; can only introspect to module or
+                # function level, not any deeper than that
+                else:
+                    print("{0}: error: cannot introspect beyond function level"
+                          "".format(C_INFO))
+                    return
+        # else if the first argument is C_INFO_INP (input example stuff)
+        elif args[0] == C_INFO_INP:
+            # if there is only one argument
+            if argc == 1:
+                # get sorted list from _INPUT_LIST keys and add 4-space indents
+                ilist = sorted(list(_INPUT_LIST.keys()))
+                for i in range(len(ilist)):
+                    ilist[i] = "    " + ilist[i]
+                # join by newline
+                ilist = "\n".join(ilist)
+                # print out list of sample inputs
+                print("List of sample inputs:\n\n{0}".format(ilist))
+            # else there are two arguments, so try to retrieve the requested
+            # example input
+            else:
+                # try to find the dictionary entry by key
+                ient = args[1]
+                try: ient = _INPUT_LIST[ient]
+                except KeyError:
+                    print("{0}: error: sample input '{1}' not in input list"
+                          "".format(C_INFO, ient))
+                    return
+                except:
+                    _ufatal(C_INFO)
+                    return
+                # if successful, print info and view
+                print(_strwrap("Info on sample input {0}:\n\n{1}"
+                      "".format(ient[_ENAME], ient[_EBLURB])))
+        # else unknown subcommand, so print error and exit
+        else:
+            print("{0}: error: unknown subcommmand '{1}' type '{2} {0}' for "
+                  "usage.".format(C_INFO, args[0], C_HELP))
+            return
+        # if there was only one argument, print this line too
+        if argc == 1:
+            print("\nFor info on {0}, type '{1} {0}'".format(C_INFO, C_HELP))
+    # too many arguments, so print error again
+    else:
+        print("{0}: error: too many arguments. type '{1} {0}' for usage."
+              "".format(C_INFO, C_HELP))
     return
 
 # main
@@ -374,8 +540,14 @@ if __name__ == "__main__":
     thismodule = graph
     print(getattr(thismodule, "bfs")(src, u_dag01_al, get_path = True))
     print(str(inspect.signature(graph.bfs)).strip("()"))
-    print(_ENAME)
     # print some informative message here
+
+    # if the package is not in the same directory, print error and exit
+    if os.path.isdir(_PACKAGE_PATH + _PACKAGE_NAME) == False:
+        print(_strwrap("{0}: fatal error: is is required that package {1} and "
+                       "{0} be located in the same directory."
+                       "".format(_PROGNAME, _PACKAGE_NAME)), file = sys.stderr)
+        quit(1)
     # main interpreter line
     while True:
         # prompt for input, save to string, parse arguments (crudely)
@@ -402,4 +574,4 @@ if __name__ == "__main__":
         # else if the command is the test command
         elif arglist[0] == C_TEST: _test(arglist[1:])
             # else if the command is the info command
-        elif arglist[0] == C_INFO: pass
+        elif arglist[0] == C_INFO: _info(arglist[1:])
